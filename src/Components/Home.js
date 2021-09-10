730c113a70904c3c9c666eb289e9aea16b821047
chinoyeh <ruthochons@gmail.com>
2021-09-10 11:47:06 +0100

    first

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+const Home = () => {
+    // const [date, setDate] = useState("");
+    // var today;
+
+
+    return (
+        <div>
+            <h1>Welcome! </h1>
+        </div>
+    )
+}
+
+export default Home
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {  useHistory } from 'react-router-dom';
+import './index.css'
 
 const Login = () => {
+    const [password, setPassword] = useState("");
+    const [user, setUser] = useState ("");
+    const [error, setError] = useState("");
+    const history = useHistory();
+    const handleLogin = (e)=>{
+        e.preventDefault();
+        
+        if (password === "chinoyeh"&& user ==='chinoyeh' ){
+            history.push('/Home');
+        }
+        else if (password ==="" ||user === ""){
+            setError('Feild Cannot be Empty')
+        }
+        else if (password !=="chinoyeh" ||user !=="chinoyeh"  ){
+            setError ('Wrong details')
+        }
+ 
+    }
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+     
+     
+   <div className = 'container'>
+            <div className ="Login">
+       
+       <div className ="Login-image">
+           <h1> Welcome  </h1>
+       </div>
+       <div >
+           <form className ="login-form">
+               <h1>Login</h1>
+             
+               <label htmlFor ="name">Username: </label>
+               <input type = "name" id ="name" name ="email" value ={user}   onChange={(e) => setUser(e.target.value)}/>                    
+               <label htmlFor = "password">Password: </label>
+               <input type ="password" id = "password" name ="password" value ={password}   onChange={(e) => setPassword(e.target.value)}/>
+               <button  type = 'submit' onClick ={handleLogin}> Login </button>
+               <p style ={{color: "red", marginTop: "10px"}}> {error}</p>
+               {/* <p>Don't have an account <Link to  ='/signup'>Sign up</Link></p> */}
+           </form>
+
+
+   </div>
+   
+</div>
+   </div>
+
+
     )
 }
 
 export default Login
+
