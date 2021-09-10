048dfc8568043052788e0ae75fd48f084f45125d
Chinoye Ochonogor <ruthochons@gmail.com>
2021-09-10 21:34:10 +0100

    Update Health.js

--- src/Components/Health.js
+++ src/Components/Health.js
@@ -6,14 +6,14 @@ const Health = () => {
         <div className ="Productivity">
                  <div className ='header-productivity'>
         
-       <Link to ='/Home'> <h3>Back</h3> </Link>
-       <Link to ='/Finances'> <h3>Next</h3> </Link>
+       <Link to ='/Worry'> <h3>Back</h3> </Link>
+       
         </div> 
             
             <form className ="productivity-form">
                 <h3> How is your mental Health today?</h3>
                 <input style ={{width: '300px'}} type ='text'/>
-               <p> <button> View Today</button></p>
+              <Link to ='/Home'><p> <button> View Today</button></p></Link>
 
             </form>
         </div>
